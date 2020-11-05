9e3a07b924b2c7cab2bbf70176fb27faac7b9423
Дмитрий Ромась <ramzzila97@gmail.com>
2020-11-05 10:59:37 +0200

    Init api for database "Back"

--- Restaurant.Back.Api/Startup.cs
+++ Restaurant.Back.Api/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Restaurant.Back.Infrastructure.Extensions;
 
 namespace Restaurant.Back.Api
 {
@@ -26,6 +27,8 @@ namespace Restaurant.Back.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.InitArchitecture();
+            services.InitDb(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
