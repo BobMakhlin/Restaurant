2431170e30b795bf5d96f872ab598eeaee4ee14b
Bob Makhlin <borismakhlin5@gmail.com>
2020-11-09 16:05:10 +0200

    Add "Post" and "Delete" to ProductController to "Front" API

--- Restaurant.Front.Api/Controllers/ProductController.cs
+++ Restaurant.Front.Api/Controllers/ProductController.cs
@@ -16,9 +16,14 @@ namespace Restaurant.Front.Api.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        #region Fields
         ICrudService<ProductDto, int> m_productService;
         ICrudService<ProductIngredientDto, int> m_productIngredientService;
         ICrudService<ProductLabelDto, int> m_productLabelService;
+        ProductIngredientHelper m_productIngredientHelper;
+        ProductLabelHelper m_productLabelHelper;
+        #endregion
+
 
         public ProductController
         (
@@ -30,6 +35,9 @@ namespace Restaurant.Front.Api.Controllers
             m_productService = productService;
             m_productIngredientService = productIngredientService;
             m_productLabelService = productLabelService;
+
+            m_productIngredientHelper = new ProductIngredientHelper(m_productIngredientService);
+            m_productLabelHelper = new ProductLabelHelper(m_productLabelService);
         }
 
 
@@ -65,6 +73,44 @@ namespace Restaurant.Front.Api.Controllers
         }
 
 
+        [HttpPost]
+        public async Task<ActionResult<ProductDto>> PostProduct(ProductDto product)
+        {
+            var transaction = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+
+            try
+            {
+                var addedProduct = await m_productService.AddAsync(product);
+                var productId = addedProduct.Id;
+
+
+                await m_productIngredientHelper.AddIngredientsAsync(productId, product.Ingredients);
+                await m_productLabelHelper.AddLabelsAsync(productId, product.Labels);
+
+
+                var target = await m_productService.GetAsync(productId);
+
+
+                transaction.Complete();
+
+                return CreatedAtAction
+                (
+                    nameof(GetProduct),
+                    new { Id = productId },
+                    target
+                );
+            }
+            catch (Exception)
+            {
+                return StatusCode(500);
+            }
+            finally
+            {
+                transaction.Dispose();
+            }
+        }
+
+
         [HttpPut("{id}")]
         public async Task<ActionResult<ProductDto>> PutProduct(int id, ProductDto product)
         {
@@ -74,23 +120,19 @@ namespace Restaurant.Front.Api.Controllers
             }
 
 
-            //var transaction = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+            var transaction = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
 
             try
             {
                 await m_productService.UpdateAsync(product);
 
 
-                var productIngredientHelper = new ProductIngredientHelper(m_productIngredientService);
-
-                await productIngredientHelper.DeleteIngredientsAsync(product.Id);
-                await productIngredientHelper.AddIngredientsAsync(product.Id, product.Ingredients);
+                await m_productIngredientHelper.DeleteIngredientsAsync(product.Id);
+                await m_productIngredientHelper.AddIngredientsAsync(product.Id, product.Ingredients);
 
 
-                var productLabelHelper = new ProductLabelHelper(m_productLabelService);
-
-                await productLabelHelper.DeleteLabelsAsync(product.Id);
-                await productLabelHelper.AddLabelsAsync(product.Id, product.Labels);
+                await m_productLabelHelper.DeleteLabelsAsync(product.Id);
+                await m_productLabelHelper.AddLabelsAsync(product.Id, product.Labels);
 
 
 
@@ -98,13 +140,51 @@ namespace Restaurant.Front.Api.Controllers
 
 
 
-                //transaction.Complete();
+                transaction.Complete();
 
                 return Ok(insertedProduct);
             }
             catch (Exception ex)
             {
-                return StatusCode(500);
+                return StatusCode(500, ex);
+            }
+            finally
+            {
+                transaction.Dispose();
+            }
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteProduct(int id)
+        {
+            var transaction = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+
+            try
+            {
+                var target = await m_productService.GetAsync(id);
+
+                if (target == null)
+                {
+                    return NotFound();
+                }
+
+                await m_productIngredientHelper.DeleteIngredientsAsync(target.Id);
+                await m_productLabelHelper.DeleteLabelsAsync(target.Id);
+                await m_productService.DeleteAsync(target);
+
+
+                transaction.Complete();
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex);
+            }
+            finally
+            {
+                transaction.Dispose();
             }
         }
     }
