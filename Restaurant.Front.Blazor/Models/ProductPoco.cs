908549c1ea9d009c9fe0faefa5c83b3aeb9e1a3a
Дмитрий Ромась <ramzzila97@gmail.com>
2020-11-10 16:43:46 +0200

    Add blazor front. Show all products.

---
+++ Restaurant.Front.Blazor/Models/ProductPoco.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Restaurant.Front.Blazor.Models
+{
+    public class ProductPoco
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Photo { get; set; }
+        public decimal Price { get; set; }
+        public List<LabelPoco> Labels { get; set; }
+    }
+}
