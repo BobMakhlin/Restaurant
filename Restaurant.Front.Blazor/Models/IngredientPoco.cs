932a2053d39fc5743a1dce52bf34494a2e914dfb
Дмитрий Ромась <ramzzila97@gmail.com>
2020-11-11 12:15:47 +0200

    Add ProductDetails and Loading

--- Restaurant.Front.Blazor/Models/IngredientPoco.cs
+++ Restaurant.Front.Blazor/Models/IngredientPoco.cs
@@ -1,22 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Restaurant.Front.Blazor.Models
 {
-    public class ProductPoco
+    public class IngredientPoco
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public string Description { get; set; }
-        public double Weight { get; set; }
         public string Photo { get; set; }
         public decimal Price { get; set; }
         public bool IsEnabled { get; set; }
-        public List<LabelPoco> Labels { get; set; }
-        public List<IngredientPoco> Ingredients { get; set; }
     }
 }
