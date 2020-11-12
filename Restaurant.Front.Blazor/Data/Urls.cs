e08f061b819820eb98da960b2f3dccad30e12db1
Bob Makhlin <borismakhlin5@gmail.com>
2020-11-12 14:37:44 +0200

    Move api-links into the class Urls

---
+++ Restaurant.Front.Blazor/Data/Urls.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Restaurant.Front.Blazor.Data
+{
+    public class Urls
+    {
+        public const string FrontApi = "https://localhost:44390/api";
+        public const string FrontApiProduct = FrontApi + "/product";
+    }
+}
